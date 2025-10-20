4b35262a8c65d4915463ca51ee568a62ac2b07c9
Vaatsal <vaatsalyasrivastava_ee_25a19036@dtu.ac.in>
2025-10-20 18:47:15 +0530

    Refactor encoding logic for categorical variables

--- P1.py
+++ P1.py
@@ -2,21 +2,21 @@ import pandas as pd
 import numpy as np
 data=pd.read_csv("C:/Users/vaats/Downloads/employee_categorical_data.csv")
 X=data
-X1=X.copy()
+X1=data.copy()
 categorycolumns=X1.select_dtypes(include="object").columns
 for col in categorycolumns:
     X1[col+'_encoded']=X1[col].astype('category').cat.codes
     X1=X1.drop(col,axis=1)
+    print(X1.head())
     # List comprehension method 
     X2=data.copy()
-    Employment_encodings={
-        "full_time":0,
-        "part_time":1,
-        "contract":2,
-        "intern":3
+    Gender_encodings={
+      "Male":0,
+      "Female":1,
+      "Other":2
     }
     X2=data.copy()
-    X2["Employment_Type_Encoded"]=X2['Employment_Type'].map(Employment_encodings)
-    X2.drop("Employment_Type",axis=1)
+    X2["Gender_Encoded"]=X2['Gender'].map(Gender_encodings)
+    X2.drop("Gender",axis=1)
     print(X2.head())
     
