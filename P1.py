5963b0c95a9ab8874b34dd9119e27dc2fb8528a8
Vaatsal <vaatsalyasrivastava_ee_25a19036@dtu.ac.in>
2025-10-20 18:13:27 +0530

    Clean up P1.py by removing unused variable definitions
    Removed unused code for identifying categorical and numerical columns.

--- P1.py
+++ P1.py
@@ -3,13 +3,6 @@ import numpy as np
 data=pd.read_csv("C:/Users/vaats/Downloads/employee_categorical_data.csv")
 y=data.Employment_Type
 X=data.drop(['Employment_Type'],axis=1)
-cardinalitycols = [K for K in X.columns if X[K].nunique() < 10 and 
-                        X[K].dtype == "object"]
-numerical_cols = [M for M in X.columns if X[M].dtype in ['int64', 'float64']]
-boolseries = (X.dtypes == 'object')
-object_cols = list(boolseries[boolseries].index)
-print("Categorical variables:")
-print(object_cols)
 X1=X.copy()
 categorycolumns=X1.select_dtypes(include="object").columns
 for col in categorycolumns:
