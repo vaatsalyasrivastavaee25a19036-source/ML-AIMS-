24444cfacfee187b8028259992f31596ed9def28
Vaatsal <vaatsalyasrivastava_ee_25a19036@dtu.ac.in>
2025-10-21 13:42:00 +0530

    Add files via upload

---
+++ OneHotEncoding.py
@@ -0,0 +1,15 @@
+import numpy as np 
+import pandas as pd 
+data=pd.read_csv("C:/Users/vaats/Downloads/employee_categorical_data.csv")
+X=data
+X1=X.copy()
+datacolumn = X1['Performance_Rating'].astype('category')
+codes = datacolumn.cat.codes
+categories = datacolumn.cat.categories
+numcategories = len(categories)
+numsamples = len(datacolumn)
+OneHotarray = np.identity(numcategories)[codes]
+columnnames = [f'Dept_{cat}' for cat in categories]
+OneHotDf = pd.DataFrame(OneHotarray, columns=columnnames, index=X1.index)
+Xencoded = pd.concat([X1.drop('Department', axis=1), OneHotDf], axis=1)
+print(Xencoded.head())
