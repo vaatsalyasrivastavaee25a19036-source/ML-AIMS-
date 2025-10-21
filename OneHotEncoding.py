68ea58954dfab2d7fa9fb35e648389c43178e2f1
Vaatsal <vaatsalyasrivastava_ee_25a19036@dtu.ac.in>
2025-10-21 15:45:16 +0530

    Update OneHotEncoding.py

--- OneHotEncoding.py
+++ OneHotEncoding.py
@@ -3,15 +3,16 @@ import pandas as pd
 data=pd.read_csv("C:/Users/vaats/Downloads/employee_categorical_data.csv")
 X=data
 X1=X.copy()
-datacolumn = X1['Performance_Rating'].astype('category')
-codes = datacolumn.cat.codes
-categories = datacolumn.cat.categories
+column = X1['Performance_Rating'].astype('category')
+codes = column.cat.codes
+categories = column.cat.categories
 numcategories = len(categories)
-numsamples = len(datacolumn)
-OneHotarray = np.identity(numcategories)[codes]
+numsamples = len(column)
+array = np.identity(numcategories)[codes]
 columnnames = [f'Dept{cat}'for cat in categories]
-OneHotDf = pd.DataFrame(OneHotarray,columns=columnnames,index=X1.index)
-Xencoded = pd.concat([X1.drop('Department',axis=1),OneHotDf],axis=1)
+Df = pd.DataFrame(array,columns=columnnames,index=X1.index)
+Xencoded = pd.concat([X1.drop('Department',axis=1),Df],axis=1)
 print(Xencoded.head())
+
 
 
